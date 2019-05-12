3bbdfaa7cdb43e1e13c9016c009cfbe92817e5f6
Karl Hughes <khughes.me@gmail.com>
2019-05-12 12:35:19 -0500

    Adding global alert

--- src/components/GlobalAlert/index.js
+++ src/components/GlobalAlert/index.js
@@ -12,18 +12,10 @@ class GlobalAlert extends React.Component {
   }
 
   render = () => (
-    <div className="">
+    <div>
       {this.state.status === statuses.ERROR ? (
         <div className="alert alert-danger">
-          <strong>Whoops! Something went wrong.</strong> Check the console to
-          learn more or contact support for help.
-        </div>
-      ) : (
-        ''
-      )}
-      {this.state.status === statuses.SAVED ? (
-        <div className="alert alert-success">
-          <strong>Account saved!</strong> You can now carry on with your day.
+          <strong>Whoops! Something went wrong.</strong>
         </div>
       ) : (
         ''
