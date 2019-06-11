963fc9fd050e3564e0ec6795c8462dce18ff4760
Karl Hughes <khughes.me@gmail.com>
2019-06-11 14:50:01 -0500

    Showing alert when clicked

--- src/components/Conference/index.js
+++ src/components/Conference/index.js
@@ -4,68 +4,130 @@ import { Link } from '@reach/router'
 import ConferenceButtonGroup from '../ConferenceButtonGroup'
 import moment from 'moment'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
+import { statuses } from '../../utilities/statuses'
 
-const Conference = ({ data, hideButtons, auth }) => {
-  const {
-    name,
-    cfp_days_until,
-    cfp_url,
-    cfp_due_date,
-    icon,
-    location,
-    category,
-    is_new,
-    event_start_date,
-  } = data
-  const isClosed = moment(cfp_due_date)
-    .endOf('day')
-    .isBefore(moment().startOf('day'))
-  const is_pro = cfp_days_until > 21
+class Conference extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: statuses.READY,
+    }
+  }
 
-  return (
-    <li className="list-group-item">
-      <div className="mt-1">
-        {categoryBadge(category)}
-        {newBadge(is_new)}
-        {proBadge(is_pro)}
-        <OutboundLink href={cfp_url} target="_blank">
-          <h3>
-            <div className="pull-left">
-              {icon && icon[0] && icon[0].url ? (
-                <img
-                  src={icon[0].url}
-                  style={{
-                    display: 'block',
-                    margin: '0 1rem 0 0',
-                    height: '24px',
-                    width: '24px',
-                  }}
-                />
-              ) : (
-                ''
-              )}
+  render = () => {
+    const {
+      name,
+      cfp_days_until,
+      cfp_url,
+      cfp_due_date,
+      icon,
+      location,
+      category,
+      is_new,
+      isSaved,
+      isTracked,
+      isHidden,
+      event_start_date,
+    } = this.props.data
+    const isClosed = moment(cfp_due_date)
+      .endOf('day')
+      .isBefore(moment().startOf('day'))
+    const is_pro = cfp_days_until > 21
+
+    return (
+      <>
+        <li
+          className={
+            this.state.status === statuses.CLICKED
+              ? 'list-group-item mb-0'
+              : 'list-group-item'
+          }
+        >
+          <div className="mt-1">
+            {categoryBadge(category)}
+            {newBadge(is_new)}
+            {proBadge(is_pro)}
+            <OutboundLink
+              href={cfp_url}
+              target="_blank"
+              onClick={this.showClickMessage}
+            >
+              <h3>
+                <div className="pull-left">
+                  {icon && icon[0] && icon[0].url ? (
+                    <img
+                      src={icon[0].url}
+                      style={{
+                        display: 'block',
+                        margin: '0 1rem 0 0',
+                        height: '24px',
+                        width: '24px',
+                      }}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+                {name}
+              </h3>
+            </OutboundLink>
+            <p className={isClosed ? 'text-muted' : ''}>
+              <strong>CFPs {isClosed ? 'Closed' : 'Due'}:</strong>{' '}
+              <time dateTime={cfp_due_date}>{cfp_due_date}</time>
+            </p>
+            <p>
+              <strong>Perks:</strong> {perksList(this.props.data)}
+            </p>
+            <p>
+              <strong>Conference Date:</strong>{' '}
+              <time dateTime={event_start_date}>{event_start_date}</time>
+            </p>
+            <p>
+              <strong>Location:</strong> {location}
+            </p>
+            {this.props.hideButtons ? (
+              ''
+            ) : (
+              <ConferenceButtonGroup
+                data={this.props.data}
+                auth={this.props.auth}
+              />
+            )}
+          </div>
+        </li>
+        {this.state.status === statuses.CLICKED ? (
+          isTracked || isHidden ? (
+            ''
+          ) : isSaved ? (
+            <div className="alert alert-dismissible alert-warning">
+              <strong>Did you apply?</strong> Don't forget to{' '}
+              <strong>
+                <i className="fa fa-compass" /> Track
+              </strong>{' '}
+              this CFP.
             </div>
-            {name}
-          </h3>
-        </OutboundLink>
-        <p className={isClosed ? 'text-muted' : ''}>
-          <strong>CFPs {isClosed ? 'Closed' : 'Due'}:</strong>{' '}
-          <time dateTime={cfp_due_date}>{cfp_due_date}</time>
-        </p>
-        <p>
-          <strong>Perks:</strong> {perksList(data)}
-        </p>
-        <p>
-          <strong>Conference Date:</strong>{' '}
-          <time dateTime={event_start_date}>{event_start_date}</time>
-        </p>
-        <p>
-          <strong>Location:</strong> {location}
-        </p>
-        {hideButtons ? '' : <ConferenceButtonGroup data={data} auth={auth} />}
-      </div>
-    </li>
-  )
+          ) : (
+            <div className="alert alert-dismissible alert-warning">
+              <strong>Like this one?</strong> Click{' '}
+              <strong>
+                <i className="fa fa-star-o" /> Save
+              </strong>{' '}
+              to get reminders before it's due.
+            </div>
+          )
+        ) : (
+          ''
+        )}
+      </>
+    )
+  }
+
+  showClickMessage = e => {
+    this.setState({
+      ...this.state,
+      status: statuses.CLICKED,
+    })
+  }
 }
 
 function newBadge(is_new) {
--- src/utilities/statuses.js
+++ src/utilities/statuses.js
@@ -4,4 +4,5 @@ export const statuses = {
   SAVING: 'saving',
   SAVED: 'saved',
   ERROR: 'error',
+  CLICKED: 'clicked',
 }
