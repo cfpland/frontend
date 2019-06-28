3d3894c0d9cee280daf75048c031c618b8a78b16
Karl Hughes <khughes.me@gmail.com>
2019-06-28 18:27:16 -0500

    Adding summary

--- src/components/UserSummary/index.js
+++ src/components/UserSummary/index.js
@@ -1,20 +1,61 @@
 import React from 'react'
 import './style.scss'
 import { Link } from 'gatsby'
+import ApiClient from '../../utilities/api-client'
 
 class UserSummary extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this._isMounted = false
+    this.state = {
+      openCfps: null,
+      savedCfps: null,
+      appliedCfps: null,
+    }
+    this.apiClient = new ApiClient()
+  }
+
+  componentDidMount = () => {
     const { auth } = this.props
+    this._isMounted = true
+
+    if (this.apiClient.isAuthenticated) {
+      this.apiClient.getSavedConferences().then(userConferences => {
+        this.setState({
+          ...this.state,
+          savedCfps: userConferences.data.items.filter(
+            uc => uc.action === 'saved'
+          ).length,
+          appliedCfps: userConferences.data.items.filter(
+            uc =>
+              uc.action === 'tracked' && uc.meta.trackingStatus === 'applied'
+          ).length,
+        })
+      })
+      this.apiClient.getConferences().then(conferences => {
+        this.setState({
+          ...this.state,
+          openCfps: conferences.data.total,
+        })
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+  }
+
+  render() {
     return (
       <div id="userSummary" className="container mt-1 mt-md-5">
-        <h1>Your CFP Land Pro Summary</h1>
+        <h1>Your Usage Summary</h1>
         <div className="row">
           <div className="col-md-4">
             <div className="card text-center mt-3">
               <div className="card-header">Open CFPs</div>
               <div className="card-body">
                 <Link className="total text-info" to="/c/all/">
-                  120
+                  {this.state.openCfps || '...'}
                 </Link>
               </div>
               <div className="card-footer">
@@ -30,7 +71,7 @@ class UserSummary extends React.Component {
               <div className="card-header">Saved CFPs</div>
               <div className="card-body">
                 <Link className="total text-warning" to="/c/saved/">
-                  9
+                  {this.state.savedCfps === null ? '...' : this.state.savedCfps}
                 </Link>
               </div>
               <div className="card-footer">
@@ -46,7 +87,9 @@ class UserSummary extends React.Component {
               <div className="card-header">Applied</div>
               <div className="card-body">
                 <Link className="total text-success" to="/c/applied/">
-                  5
+                  {this.state.appliedCfps === null
+                    ? '...'
+                    : this.state.appliedCfps}
                 </Link>
               </div>
               <div className="card-footer">
