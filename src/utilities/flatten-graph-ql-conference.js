671ed18c6a33ef8ea4544bacc766f8ca7c1160a2
Karl Hughes <khughes.me@gmail.com>
2019-07-17 12:28:15 -0500

    Adding airtable id to conferences

--- src/utilities/flatten-graph-ql-conference.js
+++ src/utilities/flatten-graph-ql-conference.js
@@ -1,6 +1,7 @@
 export const flattenGraphqlConference = gqlConference => {
   return {
     ...gqlConference.node.data,
+    providerId: gqlConference.node.data.record_id,
     category: gqlConference.node.data.category[0].data.name,
   }
 }
