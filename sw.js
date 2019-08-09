aa4114db297be5de60231811823927d154e02c9a
Karl Hughes <khughes.me@gmail.com>
2019-08-09 13:14:08 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,17 +26,17 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3bb1777276d1fd1e563b.js"
+    "url": "webpack-runtime-4874428d69b13bff4077.js"
   },
   {
-    "url": "app-bf31b9f33d7acea98ef1.js"
+    "url": "app-fce3d674ba2caf2b0489.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-665cc286673a3e4e5cb9.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fdfde5a7fbdedf71a357ec2a77933706"
+    "revision": "4d2f84ecfed78035e50a9484defbf783"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
