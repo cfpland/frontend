a72c12291a02da7d8400e49ddc200f3d2324a983
autodeployer <autodeployer@pcto.co>
2019-09-15 23:53:52 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b3ba41420817bd5ea17c.js"
+    "url": "webpack-runtime-5577afe0ba58e32c6067.js"
   },
   {
     "url": "app-b0085d222d6b993619b3.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "57adc53ea035784360ec44eaee6c660f"
+    "revision": "4e6c628cc54beb38121a933771216856"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
